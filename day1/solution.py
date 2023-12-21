23e03d72e1521d392fc6923936dbf8f2d1b1f7ef
P James <22-james@users.noreply.pelib.net>
2023-12-21 15:09:53 +0800

    day1 part2

--- day1/solution.py
+++ day1/solution.py
@@ -1,3 +1,6 @@
+import os
+
+
 def calibration(line: str):
     digits = []
     for charc in line:
@@ -19,7 +22,79 @@ def trebuchet(_input: str) -> int:
     return ret
 
 
-if __name__ == "__main__":
-    import sys
+class State:
+    def __init__(self):
+        self.value_lookup = {
+            1: "one",
+            2: "two",
+            3: "three",
+            4: "four",
+            5: "five",
+            6: "six",
+            7: "seven",
+            8: "eight",
+            9: "nine",
+        }
+        self.state_map = {k: list(v) for k, v in self.value_lookup.items()}
 
-    print(f"Result: {trebuchet(sys.argv[1])}")
+    def add(self, charc: str) -> int:
+        if charc in "123456789":
+            self.reset_all()
+            return int(charc)
+
+        result = 0
+        for k, v in self.state_map.items():
+            if charc == v[0]:
+                v.pop(0)
+                if len(v) == 0:
+                    self.reset_k(k)
+                    result = k
+            else:
+                self.reset_k(k)
+
+        return result
+
+    def reset_all(self):
+        for key in self.state_map:
+            self.reset_k(key)
+
+    def reset_k(self, key):
+        if len(self.value_lookup[key]) != len(self.state_map[key]):
+            self.state_map[key] = list(self.value_lookup[key])
+
+
+def calibration2(perline: str) -> int:
+    state = State()
+    digits = []
+    for charc in perline:
+        ret = state.add(charc)
+        if ret:
+            digits.append(ret)
+
+    if len(digits) == 1:
+        return 10 * digits[0] + digits[0]
+    elif len(digits) >= 2:
+        return 10 * digits[0] + digits[-1]
+    else:
+        raise RuntimeError(f"Unknown line: {perline}")
+
+
+def trebuchet2(puzzle_input: str) -> int:
+    """
+    no pass lower than the right
+    i have no idea of this
+    """
+    ret = 0
+    for line in iter(puzzle_input.split("\n")):
+        calibration_ = calibration2(line)
+        # print(f"num: {calibration_} of '{line}'")
+        ret += calibration_
+
+    return ret
+
+
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    with open(f"{current_dir}/puzzle_input") as input_file:
+        # print(trebuchet(input_file.read()))
+        print(trebuchet2(input_file.read()))
--- day1/test_day1.py
+++ day1/test_day1.py
@@ -1,4 +1,4 @@
-from .solution import trebuchet
+from .solution import trebuchet, trebuchet2
 
 
 def test_trebuchet():
@@ -8,3 +8,14 @@ a1b2c3d4e5f
 treb7uchet"""
     output = trebuchet(_input)
     assert output == 142
+
+
+def test_part2():
+    puzzle_input = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+    assert 281 == trebuchet2(puzzle_input)
