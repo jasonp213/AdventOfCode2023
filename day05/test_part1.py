612078eaf84a6767213d8819ae7283d3e1a9a320
P James <22-james@users.noreply.pelib.net>
2023-12-27 11:01:36 +0800

    update

---
+++ day05/test_part1.py
@@ -0,0 +1,44 @@
+import textwrap
+
+
+def solution(puzzle):
+    pass
+
+
+def test_part1():
+    _puzzle = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4"""
+    puzzle = textwrap.dedent(_puzzle).strip()
+    assert solution(puzzle) == 35
